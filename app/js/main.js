2d10ce3bb8671a41c14d6b657f36907d6989abf0
Rodion <wead758@gmail.com>
2023-11-07 06:26:31 +0300

    added sticky header and contacts link in nav menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,34 @@
 $(function () {
+  // Header menus and submenus
 
-    // Header menus and submenus
+  $(".header-menu__item").on("click", function () {
+    const $submenuWrapper = $(this).find(".header-submenu__wrapper");
 
-    $('.header-menu__item').on('click', function () {
-        const $submenuWrapper = $(this).find('.header-submenu__wrapper');
-        
-        
-        if ($submenuWrapper.hasClass('header__submenu--active')) {
-            $submenuWrapper.removeClass('header__submenu--active');
-        } else {
-          
-            $('.header-submenu__wrapper').removeClass('header__submenu--active');
-            
-         
-            $submenuWrapper.addClass('header__submenu--active');
-        }
-    });
+    if ($submenuWrapper.hasClass("header__submenu--active")) {
+      $submenuWrapper.removeClass("header__submenu--active");
+    } else {
+      $(".header-submenu__wrapper").removeClass("header__submenu--active");
 
-    
-    $(document).on('click', function (e) {
-        if (!$(e.target).closest('.header-menu__item').length && !$(e.target).closest('.header-submenu__wrapper').length) {
-            $('.header-submenu__wrapper').removeClass('header__submenu--active');
-        }
-    });
+      $submenuWrapper.addClass("header__submenu--active");
+    }
+  });
 
-   
-    $('.header-submenu__wrapper').on('mouseleave', function () {
-        $(this).removeClass('header__submenu--active');
-    });
+  $(document).on("click", function (e) {
+    if (
+      !$(e.target).closest(".header-menu__item").length &&
+      !$(e.target).closest(".header-submenu__wrapper").length
+    ) {
+      $(".header-submenu__wrapper").removeClass("header__submenu--active");
+    }
+  });
+
+  $(".header-submenu__wrapper").on("mouseleave", function () {
+    $(this).removeClass("header__submenu--active");
+  });
+
+  window.onscroll = function () {
+    myFunction();
+  };
+
 
 });
