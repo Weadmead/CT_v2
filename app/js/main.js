56d3d7641508b3e0479d8b56523483b9859f3ad9
Rodion <wead758@gmail.com>
2023-11-13 03:14:02 +0300

    finish layout on hero section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,71 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelector(".tabs");
+  const tabsBtn = document.querySelectorAll(".tabs__btn");
+  const tabsContent = document.querySelectorAll(".tabs__content");
+
+  if (tabs) {
+    tabs.addEventListener("click", (e) => {
+      if (e.target.classList.contains("tabs__btn")) {
+        const tabsPath = e.target.dataset.tabsPath;
+        tabsBtn.forEach((el) => {
+          el.classList.remove("tabs__btn--active");
+        });
+        document
+          .querySelector(`[data-tabs-path="${tabsPath}"]`)
+          .classList.add("tabs__btn--active");
+        tabsHandler(tabsPath);
+      }
+    });
+  }
+
+  const tabsHandler = (path) => {
+    tabsContent.forEach((el) => {
+      el.classList.remove("tabs__content--active");
+    });
+    document
+      .querySelector(`[data-tabs-target="${path}"]`)
+      .classList.add("tabs__content--active");
+  };
+});
+
+
+// select hide 
+
+document.addEventListener('DOMContentLoaded', function () {
+  var contactMethodSelectSell = document.getElementById('form__contact-method-sell');
+  var timeSelectLabel1 = document.getElementById('timeSelectLabel1');
+
+  var contactMethodSelectBuy = document.getElementById('form__contact-method-buy');
+  var timeSelectLabel2 = document.getElementById('timeSelectLabel2');
+
+  function hideAllTimeSelects() {
+    timeSelectLabel1.classList.add('select-hide');
+    timeSelectLabel2.classList.add('select-hide');
+  }
+
+  hideAllTimeSelects();
+
+  contactMethodSelectSell.addEventListener('change', function () {
+    var selectedValue = contactMethodSelectSell.value;
+
+    hideAllTimeSelects();
+
+    if (selectedValue === 'phone') {
+      timeSelectLabel1.classList.remove('select-hide');
+    }
+  });
+
+  contactMethodSelectBuy.addEventListener('change', function () {
+    var selectedValue = contactMethodSelectBuy.value;
+
+    hideAllTimeSelects();
+
+    if (selectedValue === 'phone') {
+      timeSelectLabel2.classList.remove('select-hide');
+    }
+  });
+});
+
 $(function () {
   // Header menus and submenus
 
@@ -29,6 +97,4 @@ $(function () {
   window.onscroll = function () {
     myFunction();
   };
-
-
 });
